42bf087ccd1631de0c31b1e0b2e85a8b011f6d38
ethanatique <65488662+ethanatique@users.noreply.github.com>
2025-02-19 15:50:37 +0100

    changement mode de chargement bdd

--- db/script/create.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR NOT NULL
-);
--- db/script/init.sql
+++ db/script/init.sql
@@ -1,1 +1,10 @@
-INSERT INTO users (username, password, email) VALUES ('admin', 'adminpassword', 'admin@example.com');
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR NOT NULL
+);
+
+INSERT INTO users (username, password, email) VALUES ('admin', 'scrypt:32768:8:1$6xL58chXNfRQZm7B$3ae7d35760aa403b98d6d17a9182e8c0bcce8dc841ebba023eb1d1ff8084e758b77e9c101f366ba95cbd95feeb05587d047630c339143ecb519e7bff52f59da6', 'admin@example.com');
